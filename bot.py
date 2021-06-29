58041d707fc36f36ab587ab10dc8b3b5acefa5a3
Dimitry <glasgo-seven@users.noreply.github.com>
2021-06-29 23:09:49 +0300

    first build

---
+++ bot.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import codecs
+import os
+from random import randint
+from time import time, sleep
+
+import telebot
+from telebot import types
+
+from weather import get_weather
+
+def get_credentails():
+	try:
+		f = open('./firebase.json', 'r')
+		f.close()
+		return './firebase.json'
+	except FileNotFoundError:
+		return os.getenv('BOT_FIREBASE_CREDENTAILS')
+
+import firebase_admin
+from firebase_admin import db, firestore
+from google.cloud.firestore_v1 import ArrayUnion, Increment
+
+cred_obj = firebase_admin.credentials.Certificate(get_credentails())
+default_app = firebase_admin.initialize_app(cred_obj)
+DB = firestore.client()
+
+def get_token():
+	try:
+		return open('.token', 'r').read()
+	except FileNotFoundError:
+		return os.getenv('BOT_TOKEN')
+
+def get_report_chatid():
+	try:
+		return open('.report', 'r').read()
+	except FileNotFoundError:
+		return os.getenv('BOT_REPORT_CHATID')
+
+bot = telebot.TeleBot(get_token())
+# location = None
+last_weather = None
+
+
+@bot.message_handler(commands=["start"])
+def command_start(message):
+	# global location
+	try:
+		ref = DB.collection('locations').document(str(message.from_user.id)).get().to_dict()
+		if message.date - ref['time'] > 3600:
+			raise ValueError
+		# location = ref['location']
+		bot.send_message(message.chat.id, 'Данные о местоположении уже получены.\nИспользуйте "/weather", чтобы получить данные о погоде.')
+	except:
+		keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
+		button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
+		keyboard.add(button_geo)
+		bot.send_message(message.chat.id, "Необходимо дать доступ к местоположению.", reply_markup=keyboard)
+
+
+@bot.message_handler(content_types=["location"])
+def save_location(message):
+	# global location
+	uid = str(message.from_user.id)
+	try:
+		ref = DB.collection('locations').document(uid)
+		ref.update({
+			'location'	:	{
+				'latitude'	:	message.location.latitude,
+				'longitude'	:	message.location.longitude
+			},
+			'time'	:	message.date
+		})
+	except:
+		DB.collection('locations').document(uid).set({
+			'user_id'	:	uid,
+			'location'	:	{
+				'latitude'	:	message.location.latitude,
+				'longitude'	:	message.location.longitude
+			},
+			'time'			:	message.date
+		})
+
+	keyboard = types.ReplyKeyboardRemove()
+
+	bot.send_message(message.chat.id, 'Данные получены.\nИспользуйте "/weather", чтобы получить данные о погоде.', reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['weather'])
+def command_weather(message: types.Message):
+	# global location
+	ref = DB.collection('locations').document(str(message.from_user.id)).get().to_dict()
+	msg = 'Необходимо дать доступ к местоположению командой "/start".'
+	try:
+		if message.date - ref['time'] > 3600:
+			msg = 'Данные о местоположении устарели. Обновите командой "/start".'
+			raise ValueError
+		global last_weather
+		location = ref['location']
+		last_weather = get_weather(f"{location['latitude']},{location['longitude']}")
+		bot.send_message(message.chat.id, text=f'{last_weather}\n\nЕсли произошла ошибка, отправьте отчёт командой "/report".')
+	except:
+		bot.send_message(message.chat.id, text=msg)
+	# location = None
+
+
+@bot.message_handler(commands=['report'])
+def command_report(message: types.Message):
+	if last_weather != None:
+		bot.send_message(get_report_chatid(), text=f'REPORT by {message.from_user.username} - {message.date}\n\n{last_weather}')
+	bot.send_message(message.chat.id, text='Отчёт отправлен!')
+
+
+@bot.message_handler(commands=['help'])
+def command_help(message: types.Message):
+	bot.send_message(message.chat.id, text='\
+/start - Запустить бота.\n\
+/help - Помощь.\n\
+/weather - Получить погоду.\n\
+/weather_new - Получить погоду в новой геолокации.')
+
+
+
+print('------------------------')
+print('/// BOT IS POLLING ///')
+bot.polling()
---
+++ weather.py
@@ -0,0 +1,73 @@
+WEATHER = {
+	'Sunny'						:	['Солнечно', '\U00002600'],
+	'Mostly Sunny'				:	['Преимущественно солнечно', '\U0001F324'],
+
+	'Partly Cloudy'				:	['Переменная облачность', '\U0001F325'],
+	'Mostly Cloudy'				:	['Облачно с прояснениями', '\U0001F325'],
+	'Cloudy'					:	['Облачно', '\U00002601'],
+
+	'Scattered Showers'			:	['Местами дождь', '\U0001F326'],
+	'Rain'						:	['Дождь', '\U0001F327'],
+	'Scattered Thunderstorms'	:	['Местами грозы', '\U000026C8'],
+	'Thunderstorm'				:	['Гроза', '\U0001F329'],
+
+	'Wind'						:	['Ветренно', '\U0001F32C'],
+	'Foggy'						:	['Туман', '\U0001F32B'],
+
+	'Partly Cloudy Night'		:	['Переменная облачность, ночь', '\U0001F319'],
+	'Partly Clear Night'		:	['Облачно с прояснениями, ночь', '\U0001F319'],
+	'Mostly Cloudy Night'		:	['Облачно с прояснениями, ночь', '\U0001F315'],
+	'Mostly Clear Night'		:	['Переменная облачность, ночь', '\U0001F315'],
+}
+
+from bs4 import BeautifulSoup
+import requests as req
+
+def get_weather(location):
+	resp = req.get(f"https://weather.com/ru-RU/weather/today/l/{location}")
+
+	soup = BeautifulSoup(resp.text, 'lxml')
+
+	loc = soup.find('div', id='WxuHeaderLargeScreen-header-9944ec87-e4d4-4f18-b23e-ce4a3fd8a3ba').find_all('span')
+	lang = loc[0].text
+	deg = loc[1].text[-1]
+
+	grind = soup.find('div', id='WxuCurrentConditions-main-b3094163-ef75-4558-8d9a-e35e6b9b1034')
+	now_loca = grind.h1.text
+	now_temp = grind.span.text
+	now_weat = grind.title.text
+
+	try:
+		if lang == 'RU':
+			now_weat_t = WEATHER[now_weat][0]
+		else:
+			now_weat_t = now_weat
+		now_weat_i = WEATHER[now_weat][1]
+	except:
+		now_weat_t = now_weat
+		now_weat_i = ''
+	week = soup.find('div', id='WxuDailyWeatherCard-main-bb1a17e7-dc20-421a-b1b8-c117308c6626').find_all('li')
+
+	if lang == 'RU':
+		res = f'{now_loca}\nСейчас:⠀{now_temp}{deg}\n{now_weat_i}⠀{now_weat_t}\n'
+	else:
+		res = f'{now_loca}\nNow:⠀{now_temp}{deg}\n{now_weat_i}⠀{now_weat_t}\n'
+	for we in week:
+		divs = we.find_all('div')
+		try:
+			if lang == 'RU':
+				weather_t = WEATHER[we.title.text][0]
+			else:
+				weather_t = we.title.text
+			weather_i = WEATHER[we.title.text][1]
+		except:
+			weather_t = we.title.text
+			weather_i = ''
+		if lang == 'RU':
+			res += f'\n{we.h3.text.upper()}⠀-⠀{weather_i}⠀{weather_t}\n⠀⠀День:⠀{divs[0].text}{deg}\n⠀⠀Ночь:⠀{divs[1].text}{deg}'
+		else:
+			res += f'\n{we.h3.text.upper()}⠀-⠀{weather_i}⠀{weather_t}\n⠀⠀Day:⠀{divs[0].text}{deg}\n⠀⠀Night:⠀{divs[1].text}{deg}'
+
+	return res
+
+# print(get_weather('55.75,37.58'))
